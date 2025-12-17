5701b768ba9319094eaaad2af0abee2d16a3313e
Thanush Maha <thanushmaha10@gmail.com>
2025-12-17 14:25:53 +0530

    created timeformate and minor update moviecard

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { StarIcon, Heart } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import timeFormat from "../lib/timeformat";
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
@@ -55,12 +56,12 @@ const MovieCard = ({ movie }) => {
       <p className="font-semibold mt-2 truncate">{movie.title}</p>
 
       <p className="text-sm text-gray-400 mt-2">
-        {new Date(movie.release_date).getFullYear()} •{" "}
+        {new Date(movie.release_date).getFullYear()} •
         {movie.genres
           .slice(0, 2)
           .map((genre) => genre.name)
-          .join(" | ")}{" "}
-        • {movie.runtime} mins
+          .join(" | ")}
+        • {timeFormat(movie.runtime)}
       </p>
 
       <div className="flex items-center justify-between mt-4 pb-3">
