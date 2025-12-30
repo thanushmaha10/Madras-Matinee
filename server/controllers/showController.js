02efb075917a670e204ba7aabf649a74b6cdcec6
Thanush Maha <thanushmaha10@gmail.com>
2025-12-30 16:07:44 +0530

    created now-playing API

---
+++ server/controllers/showController.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const getNowPlayingMovies = async (req, res) => {
+  try {
+    const { data } = axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      {
+        headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+      }
+    );
+
+    const movies = data.results;
+    res.json({ success: true, movies: movies });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, message: err.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { clerkMiddleware } from "@clerk/express";
 import connectDb from "./configs/db.js";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/show", showRouter);
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
